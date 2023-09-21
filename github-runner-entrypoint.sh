7fe776f1033e9c961edb9cd8e9a052ce1552d02f
Andrea Grillo <andrea.grillo@pagopa.it>
2023-09-21 15:27:45 +0200

    restore some log cuz unknown (new) error

--- github-runner-entrypoint.sh
+++ github-runner-entrypoint.sh
@@ -1,3 +1,5 @@
+printf "Before token: $REPO_URL; $GITHUB_PAT; $REGISTRATION_TOKEN_API_URL;\n"
+
 # Retrieve a short lived runner registration token using the PAT
 REGISTRATION_TOKEN="$(curl -X POST -fsSL \
   -H 'Accept: application/vnd.github.v3+json' \
@@ -6,11 +8,10 @@ REGISTRATION_TOKEN="$(curl -X POST -fsSL \
   "$REGISTRATION_TOKEN_API_URL" \
   | jq -r '.token')"
 
-./actions-runner/config.sh \
-  --url $REPO_URL \
-  --token $REGISTRATION_TOKEN \
-  --unattended \
-  --ephemeral \
-  --disableupdate
+printf "After token: $REGISTRATION_TOKEN;\n"
+
+./actions-runner/config.sh --url $REPO_URL --token $REGISTRATION_TOKEN --unattended --ephemeral --disableupdate
+
+printf "config run successfully\n"
 
 ./actions-runner/run.sh
