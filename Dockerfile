9b4868535d63d6fefc73a6115342b970edef99d1
pp-ps <pietro.stroia@pagopa.it>
2024-02-10 17:48:43 +0100

    refactor deprecated apt-key add

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,6 @@
 # from https://hub.docker.com/_/ubuntu/tags?page=1&name=22.04
 FROM ubuntu:22.04@sha256:bcc511d82482900604524a8e8d64bf4c53b2461868dac55f4d04d660e61983cb
 
-# see readme to understand wich version and use to use
-ENV ENV_GITHUB_RUNNER_VERSION="2.313.0"
-ENV ENV_GITHUB_RUNNER_VERSION_SHA=56910d6628b41f99d9a1c5fe9df54981ad5d8c9e42fc14899dcc177e222e71c4
-# https://github.com/Azure/kubelogin/blob/master/CHANGELOG.md
-ENV ENV_KUBELOGIN_VERSION=0.0.31
-ENV ENV_YQ_VERSION="v4.30.6"
-ENV NODE_MAJOR_VERSION="20"
-
 WORKDIR /
 
 COPY dockerfile-setup.sh dockerfile-setup.sh
--- dockerfile-setup.sh
+++ dockerfile-setup.sh
@@ -1,108 +1,88 @@
 #!/usr/bin/env bash
-
-echo "✅ Start apt get install base packages"
-
+# base packages
 apt-get update \
     && apt-get -y install curl git vim \
     && apt-get -y install zip unzip \
-    && apt-get -y install ca-certificates curl wget apt-transport-https lsb-release gnupg \
+    && apt-get -y install ca-certificates apt-transport-https lsb-release gnupg \
     && apt-get -y install jq \
     && apt-get satisfy "python3-pip  (<= 22.1)" -y
-    # install jq from https://stedolan.github.io/jq/download/
-
-# Test whoami
-whoami
-
-echo "✅ whoami > run as expected"
 
 #
 # Github Action runner
 #
 mkdir -p actions-runner
 cd actions-runner || exit
-
 # from https://github.com/actions/runner/releases
-GITHUB_RUNNER_VERSION="${ENV_GITHUB_RUNNER_VERSION:-2.313.0}"
-GITHUB_RUNNER_VERSION_SHA="${ENV_GITHUB_RUNNER_VERSION_SHA:-56910d6628b41f99d9a1c5fe9df54981ad5d8c9e42fc14899dcc177e222e71c4}"
-curl -o actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz -L https://github.com/actions/runner/releases/download/v${GITHUB_RUNNER_VERSION}/actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz
+GITHUB_RUNNER_VERSION="2.313.0"
+GITHUB_RUNNER_VERSION_SHA="56910d6628b41f99d9a1c5fe9df54981ad5d8c9e42fc14899dcc177e222e71c4"
+curl -fsSL https://github.com/actions/runner/releases/download/v"${GITHUB_RUNNER_VERSION}"/actions-runner-linux-x64-"${GITHUB_RUNNER_VERSION}".tar.gz -o actions-runner-linux-x64-"${GITHUB_RUNNER_VERSION}".tar.gz
 echo "${GITHUB_RUNNER_VERSION_SHA}  actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz" | sha256sum -c
-tar xzf ./actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz
-rm actions-runner-linux-x64-${GITHUB_RUNNER_VERSION}.tar.gz
-
+tar xzf ./actions-runner-linux-x64-"${GITHUB_RUNNER_VERSION}".tar.gz
+rm actions-runner-linux-x64-"${GITHUB_RUNNER_VERSION}".tar.gz
 bash bin/installdependencies.sh
-echo "✅ Installed > github action runner"
+echo "✅ Installed GitHub action runner"
+
+# keyrings folder
+mkdir -p /etc/apt/keyrings
 
 #
 # AZCLI
 #
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | tee /etc/apt/sources.list.d/azure-cli.list
-
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /etc/apt/keyrings/microsoft.gpg
+chmod go+r /etc/apt/keyrings/microsoft.gpg
+AZ_DIST=$(lsb_release -cs)
+# we build only for amd64
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_DIST main" | tee /etc/apt/sources.list.d/azure-cli.list
 apt-get update \
   && apt-get -y install azure-cli
-
 az config set extension.use_dynamic_install=yes_without_prompt
+which az >/dev/null && echo "✅ Installed az" || echo "❌ failed to install az"
 
-## Test azcli
-az --version
-echo "✅ Installed > azcli"
-
-## Node and Yarn install
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/trusted.gpg.d/nodesource.gpg
-NODE_MAJOR_VERSION="${ENV_NODE_MAJOR_VERSION:-20}"
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR_VERSION.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+#
+# Node and Yarn install
+#
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR_VERSION="20"
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR_VERSION.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt-get update \
     && apt-get -y install nodejs \
     && npm install -g yarn
-
-## Node and Yarn test
-node -v
-npm -v
-yarn -v
-echo "✅ Installed Node & Yarn"
+which node >/dev/null && echo "✅ Installed node" || echo "❌ failed to install node"
+which npm >/dev/null && echo "✅ Installed npm" || echo "❌ failed to install npm"
+which yarn >/dev/null && echo "✅ Installed yarn" || echo "❌ failed to install yarn"
 
 #
 # KUBERNETES DEPENDENCIES
 #
 # install kubectl from https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
-
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
-
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 # install helm from https://helm.sh/docs/intro/install/#from-apt-debianubuntu
-curl https://baltocdn.com/helm/signing.asc | apt-key add -
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
-
+curl -fsSL https://baltocdn.com/helm/signing.asc | gpg --dearmor -o /etc/apt/keyrings/helm.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
 apt-get update
-echo "✅ Configure kubernetes & Helm for installation"
-
-
 apt-get satisfy "kubectl" -y
-## Test kubectl
-kubectl --help
-echo "✅ Installed kubernetes"
-
+which kubectl >/dev/null && echo "✅ Installed kubectl" || echo "❌ failed to install kubectl"
 apt-get satisfy "helm" -y
-## Test helm
-helm --help
-echo "✅ Installed kubernetes"
+which helm >/dev/null && echo "✅ Installed helm" || echo "❌ failed to install helm"
 
+#
 # install yq from https://github.com/mikefarah/yq#install
-YQ_VERSION="${ENV_YQ_VERSION:-v4.30.6}"
+#
+YQ_VERSION="v4.40.7"
+YQ_VERSION_SHA="b895bad59fe6a24d5c38a73d09f8b8e7cef45a5049e16504c02176ebed6d572e"
 YQ_BINARY="yq_linux_amd64"
-wget https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz -O - | tar xz && mv ${YQ_BINARY} /usr/bin/yq
-echo "✅ Installed YQ"
+curl -fsSL https://github.com/mikefarah/yq/releases/download/"${YQ_VERSION}"/${YQ_BINARY}.tar.gz -o ${YQ_BINARY}.tar.gz
+echo "${YQ_VERSION_SHA}" $YQ_BINARY.tar.gz | sha256sum -c
+mkdir -p yq && tar xz -f ${YQ_BINARY}.tar.gz -C yq && mv yq/${YQ_BINARY} /usr/bin/yq
+which yq >/dev/null && echo "✅ Installed yq" || echo "❌ failed to install yq"
 
-## Test YQ
-yq --version
-
+#
 # Kubelogin install (use kubectl to install packages)
-KUBELOGIN_VERSION="${ENV_KUBELOGIN_VERSION:-0.0.26}"
+#
+KUBELOGIN_VERSION="0.0.34"
 az aks install-cli --kubelogin-version "${KUBELOGIN_VERSION}"
-## Test kubelogin
-kubelogin --version
-echo "✅ Installed kubelogin"
+which kubelogin >/dev/null && echo "✅ Installed kubelogin" || echo "❌ failed to install kubelogin"
 
 #
 # USER CONFIGURATIONS
